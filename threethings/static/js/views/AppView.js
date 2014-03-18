fc61624c8ffeaf06bd055260a3eb84e0348766f9
Joe Hand <joe@joehand.org>
2014-03-18 14:44:07 -0600

    add notes to create acct

--- threethings/static/js/views/AppView.js
+++ threethings/static/js/views/AppView.js
@@ -11,20 +11,18 @@ define([
 ], function (Backbone, _, $) {
 
     var AppView = Backbone.View.extend({
-
         events: {
 
         },
 
         initialize: function(opt) {
-
+            this.render();
         },
 
         render: function() {
             console.log(this);
             return this;
         },
-
     });
 
     return AppView;
